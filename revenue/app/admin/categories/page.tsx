01497313e156a72b390baba6853727e4987c0b85
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-02 23:39:41 +0300

    add comment for form fields in Services Management Page

--- revenue/app/admin/categories/page.tsx
+++ revenue/app/admin/categories/page.tsx
@@ -118,6 +118,7 @@ const ServicesManagement = () => {
     }
   };
 
+  // Form Fields for Services Management Page (Admin)
   const inputClassName =
     "mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500";
   const labelClassName = "block text-sm font-medium text-gray-700 mb-1";
