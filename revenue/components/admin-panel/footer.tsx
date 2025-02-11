760f8e27eac79fafd6d506dd9da44c864b2b7459
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-11 12:21:18 +0300

    add comment to define the Footer component in footer file

--- revenue/components/admin-panel/footer.tsx
+++ revenue/components/admin-panel/footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+// Define the Footer component that will be rendered by the server and the client
 export function Footer() {
   return (
     <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
